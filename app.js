b9b9442bc3839b78ca3592b9f7259cf124310dd5
pipkyai <68229318+pipkyai@users.noreply.github.com>
2025-05-05 20:14:15 +0300

    miniapp

--- app.js
+++
@@ -1,6 +0,0 @@
-import { WebApp } from '@twa-dev/sdk';
-
-WebApp.ready(); // Инициализация
-const user = WebApp.initDataUnsafe.user; // Данные пользователя
-
-console.log(`Привет, ${user.first_name}!`);
